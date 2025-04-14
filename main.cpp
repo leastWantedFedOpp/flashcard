70e0d314d39f36b00118ab5805cedf91d4774720
Chhiring D. Lama <chhiringedu@gmail.com>
2025-04-14 10:38:04 -0700

    getting started

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FlashCard
+//
+//  Created by Chhiring D. Lama on 4/14/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
