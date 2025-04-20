c72d99bbbac1c12dfadbc1c27982d23fe02e5e2d
Chhiring D. Lama <chhiringedu@gmail.com>
2025-04-20 13:58:16 -0700

    logIn and createAccount stored in func (hungry brb)

--- main.cpp
+++ main.cpp
@@ -10,33 +10,58 @@ struct User{
     string password;
 };
 
+struct CurrentUser{
+    int id;
+    string username;
+};
+
 int genUserid(){
     srand(unsigned(time(0)));
     int randGenUserId = rand() % 9000 + 1000;
     return randGenUserId;
 }
 
-void authUser(char userInput){
+void logIn(){
     int id;
     string username, password;
-    if(userInput == 'a'){
-        cout << "Log in" << endl;
-        cout << "Username: ";
-        cin >> username;
-        cout << "Password: ";
-        cin >> password;
-        //look in userData.csv
-    } else if (userInput == 'b'){
-        cout << "Create an account" << endl;
-        id = genUserid();
-        cout << "Username: ";
-        cin >> username;
-        cout << "Password: ";
-        cin >> password;
-        //append to userData.csv
-        User newUser = {id, username, password};
-    } else {
-        cout << "Invalid input" << endl;
+    cout << "Log in" << endl;
+    cout << "Username: ";
+    cin >> username;
+    cout << "Password: ";
+    cin >> password;
+    //look in userData.csv
+    //if user is found then take that users info (id, name) and set it to CurrentUser?
+}
+
+void createAccount(){
+    int id;
+    string username, password;
+    cout << "Create an account" << endl;
+    id = genUserid();
+    cout << "Username: ";
+    cin >> username;
+    cout << "Password: ";
+    cin >> password;
+    //append to userData.csv
+    User newUser = {id, username, password};
+}
+
+
+void authUser(){
+    //open a file called Users.csv
+    //read
+    char userInput = '\0';
+    cout << "a. Login\nb.Create an account" << endl;
+    cin >> userInput;
+    switch (userInput) {
+        case 'a':
+            logIn(); //pass csv file
+            break;
+        case 'b':
+            createAccount(); //pass csv file
+            break;
+        default:
+            break;
     }
 }
 
@@ -44,18 +69,7 @@ int main(int argc, const char * argv[]) {
     char userInput = '\0';
     //working on logIn/create account
     cout << "FlashCard" << endl;
-    cout << "a. Login\nb.Create an account" << endl;
-    cin >> userInput;
-    switch (userInput) {
-        case 'a':
-            //log in
-            break;
-        case 'b':
-            //create an account
-            break;
-        default:
-            break;
-    }
+    authUser();
     return 0;
 }
 
