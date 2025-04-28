f1c7a1ff8b30b00ec5b5c16cac9059d9afbdf2bd
Chhiring D. Lama <chhiringedu@gmail.com>
2025-04-27 19:16:03 -0700

    storing userAuth functions inside a header file

---
+++ utils/userAuth.cpp
@@ -0,0 +1,8 @@
+//
+//  userAuth.cpp
+//  FlashCard
+//
+//  Created by Chhiring D. Lama on 4/27/25.
+//
+
+#include "userAuth.hpp"
---
+++ utils/userAuth.hpp
@@ -0,0 +1,13 @@
+//
+//  userAuth.hpp
+//  FlashCard
+//
+//  Created by Chhiring D. Lama on 4/27/25.
+//
+
+#ifndef userAuth_hpp
+#define userAuth_hpp
+
+#include <stdio.h>
+
+#endif /* userAuth_hpp */
