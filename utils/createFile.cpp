a172ddc73af82c84ca030723e501d1b45d45cd82
Chhiring D. Lama <chhiringedu@gmail.com>
2025-04-30 21:34:17 -0700

    storing function related to create inside a header file

---
+++ utils/createFile.cpp
@@ -0,0 +1,8 @@
+//
+//  createFile.cpp
+//  FlashCard
+//
+//  Created by Chhiring D. Lama on 4/30/25.
+//
+
+#include "createFile.hpp"
---
+++ utils/createFile.hpp
@@ -0,0 +1,13 @@
+//
+//  createFile.hpp
+//  FlashCard
+//
+//  Created by Chhiring D. Lama on 4/30/25.
+//
+
+#ifndef createFile_hpp
+#define createFile_hpp
+
+#include <stdio.h>
+
+#endif /* createFile_hpp */
