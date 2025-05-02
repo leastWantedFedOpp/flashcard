3c9f78f244bbfa443b688172e2228410b29a48a7
Chhiring D. Lama <chhiringedu@gmail.com>
2025-05-01 18:59:19 -0700

    storing functions related to review and quiz inside a header file

---
+++ utils/reviewNquiz.cpp
@@ -0,0 +1,8 @@
+//
+//  reviewNquiz.cpp
+//  FlashCard
+//
+//  Created by Chhiring D. Lama on 5/1/25.
+//
+
+#include "reviewNquiz.hpp"
---
+++ utils/reviewNquiz.hpp
@@ -0,0 +1,13 @@
+//
+//  reviewNquiz.hpp
+//  FlashCard
+//
+//  Created by Chhiring D. Lama on 5/1/25.
+//
+
+#ifndef reviewNquiz_hpp
+#define reviewNquiz_hpp
+
+#include <stdio.h>
+
+#endif /* reviewNquiz_hpp */
