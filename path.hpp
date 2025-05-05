f2a5266b29844906022d1484ecb1a6c68ca58b5d
Chhiring D. Lama <chhiringedu@gmail.com>
2025-05-05 00:47:46 -0700

    able to read file from userInfo and userData folders. Works 🙂‍↕️

---
+++ path.hpp
@@ -0,0 +1,11 @@
+#ifndef path_hpp
+#define path_hpp
+
+#include <stdio.h>
+
+extern std::filesystem::path root;
+extern std::filesystem::path userFiles; //list of files that belong to users
+extern std::filesystem::path userList; //list of users
+extern std::filesystem::path userData; //throw all users .txt fils here.
+
+#endif /* path_hpp */
